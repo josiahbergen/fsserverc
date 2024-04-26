b56d4952fc6a31af71d226c23ad534df9f17cfe6
Josiah <139285660+Jojobinx17@users.noreply.github.com>
2024-04-26 14:28:31 -0700

    handshake half works

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fsserverc
-_A server for Fresh Start written in C. Currently a work in progress!_
+
+A server for Fresh Start written in C. _Currently a work in progress!_
+
+
+## To run:
+- Clone this repository
+- Navigate to the top directory of the project (the one with README.md)
+- run `./make.sh`
--- src/server.c
+++ src/server.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <arpa/inet.h>
 #include <netdb.h>
@@ -12,79 +12,78 @@
 
 void server(unsigned short port) {
 
-    // create socket file descriptor
-    const int fd = socket(PF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        perror("Socket error");
-        return;
-    }
-
-    // set the address and port to connect to
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htons(INADDR_ANY);
-    addr.sin_port = htons(port);
-    memset(&(addr.sin_zero), '\0', 8);
-
-    // bind the socket to the address and port specified earlier
-    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr))) {
-        perror("Bind error");
-        return;
-    }
-
-    // unneeded? gets the address of the socket
-    // socklen_t addr_len = sizeof(addr);
-    // getsockname(fd, (struct sockaddr *)&addr, &addr_len);
-
-    // read on the port
-    printf("Server is running! (%s:%d)\n", inet_ntoa(addr.sin_addr),
-            (int)ntohs(addr.sin_port));
-
-    if (listen(fd, 10)) { // 10 is the maximum number of pending connections
-    perror("Listen error");
-        return;
-    }
-
-    // accept incoming connections
-    struct sockaddr_storage caddr; // sockaddr_storage is large enough to hold
-                                    // both sockaddr_in and sockaddr_in6
-
-    socklen_t caddr_len = sizeof(caddr);
-    const int cfd = accept(fd, (struct sockaddr *)&caddr, &caddr_len);
-
-    printf("A client has connected! (cfd %u)\n", cfd);
-
-    printf("Sending handshake data... ");
-    //   char data = "1";
-    //   char *datap = &data;
-    //   int bytes_sent = send(cfd, datap, 1, 0);
-
-    //   if (bytes_sent == -1) {
-    //     perror("Send error");
-    //     return;
-    //   }
-
-    //   printf("%d bytes sent. (1 expected.)\n", bytes_sent);
-
-    // read from client with recv!
-    char buf[1024];
-    int bytes_reveived = recv(cfd, buf, sizeof(buf), MSG_WAITALL);
-
-    printf("%d bytes reveived. (1024 max)", bytes_reveived);
-
-    if (bytes_reveived == 0) {
-        printf("Client disconnnected.");
-    } else if (bytes_reveived == -1) {
-        perror("Recv error");
+  // create socket file descriptor
+  const int fd = socket(PF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    perror("Socket error");
     return;
-    }
+  }
 
-    // print without a care in the world
-    printf("client says: %s\n", buf);
+  // set the address and port to connect to
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htons(INADDR_ANY);
+  addr.sin_port = htons(port);
+  memset(&(addr.sin_zero), '\0', 8);
 
-    // reset the buffer so we can use it later
-    memset((char *)&buf, 0, sizeof(buf));
+  // bind the socket to the address and port specified earlier
+  if (bind(fd, (struct sockaddr *)&addr, sizeof(addr))) {
+    perror("Bind error");
+    return;
+  }
 
-    close(cfd);
-    close(fd);
+  // unneeded? gets the address of the socket
+  // socklen_t addr_len = sizeof(addr);
+  // getsockname(fd, (struct sockaddr *)&addr, &addr_len);
+
+  // read on the port
+  printf("Server is running! (%s:%d)\n", inet_ntoa(addr.sin_addr),
+         (int)ntohs(addr.sin_port));
+
+  if (listen(fd, 10)) { // 10 is the maximum number of pending connections
+    perror("Listen error");
+    return;
+  }
+
+  // accept incoming connections
+  struct sockaddr_storage caddr; // sockaddr_storage is large enough to hold
+                                 // both sockaddr_in and sockaddr_in6
+
+  socklen_t caddr_len = sizeof(caddr);
+  const int cfd = accept(fd, (struct sockaddr *)&caddr, &caddr_len);
+
+  printf("A client has connected! (%s)\n", inet_ntoa(addr.sin_addr));
+
+  printf("Sending handshake data... ");
+  uint8_t obuffer[1] = {1};
+  int bytes_sent = send(cfd, (void *)&obuffer, sizeof(obuffer), 0);
+
+  if (bytes_sent == -1) {
+    perror("Send error");
+    return;
+  }
+
+  printf("%d byte(s) sent. (%lu expected.)\n", bytes_sent, sizeof(obuffer));
+
+  // read from client with recv!
+  char buf[1024];
+  int bytes_reveived = recv(cfd, buf, sizeof(buf), MSG_WAITALL);
+
+  printf("%d byte(s) reveived. (1024 max)\n", bytes_reveived);
+
+  if (bytes_reveived == 0) {
+    printf("Client disconnnected.");
+  } else if (bytes_reveived == -1) {
+    perror("Recv error");
+    return;
+  }
+
+  // print without a care in the world
+  // printf("client says: %s\n", buf);
+
+  // reset the buffer so we can use it later
+  memset((char *)&buf, 0, sizeof(buf));
+
+  close(cfd);
+  close(fd);
 }
